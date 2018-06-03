b4cc20ee79786f6c182f4b5c14222664c9b41f1b
Greg <gtrestour@gmail.com>
2018-06-03 12:46:58 +0200

    test map ok apres merge master

--- Projet_PLA/src/basic/Cellule.java
+++ Projet_PLA/src/basic/Cellule.java
@@ -1,9 +1,21 @@
 package basic;
+
 import principal.Entity;
+
 public class Cellule {
 	public Entity entité;
 	public boolean libre;
-	//coordonnées du centre d'une cellule
+	// coordonnées du centre d'une cellule
 	public int x;
 	public int y;
+
+	public Cellule() {
+	}
+
+	public Cellule(Entity e, boolean l, int x, int y) {
+		this.entité = e;
+		this.libre = l;
+		this.x = x;
+		this.y = y;
+	}
 }
