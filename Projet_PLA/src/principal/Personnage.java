61f4ec0822baeb05ba2ed381fd668891300a1678
Unknown <jacques7473@gmail.com>
2018-06-06 16:53:21 +0200

    Correction V2 item

--- Projet_PLA/src/principal/Personnage.java
+++ Projet_PLA/src/principal/Personnage.java
@@ -7,44 +7,41 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Personnage extends Entity {
-	BufferedImage m_sprite;
-	int m_w, m_h;
-	int m_x, m_y;
-	int m_idx;
-	float m_scale;
-	BufferedImage[] m_sprites;
-	Model m_model;
-	int orientation = 0;
+    Inventaire inventaire=new Inventaire();
+    BufferedImage m_sprite;
+    int m_w, m_h;
+    int m_x, m_y;
+    int m_idx;
+    float m_scale;
+    BufferedImage[] m_sprites;
+    Model m_model;
+    int orientation = 0;
 
-	public Personnage(Model model, BufferedImage sprite, int x, int y, float scale) {
-		m_model = model;
-		m_sprite = sprite;
-		m_x = x;
-		m_y = y;
-		m_scale = scale;
-		m_idx = 0;
-		splitSprite();
-	}
+    public Personnage(Model model, BufferedImage sprite, int x, int y, float scale) {
+        m_model = model;
+        m_sprite = sprite;
+        m_x = x;
+        m_y = y;
+        m_scale = scale;
+        m_idx = 0;
+        splitSprite();
+    }
 
-	public void splitSprite() {
-		int width = m_sprite.getWidth(null);
-		int height = m_sprite.getHeight(null);
-		m_sprites = new BufferedImage[4 * 4];
-		m_w = width / 4;
-		m_h = height / 4;
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				int x = j * m_w;
-				int y = i * m_h;
+    public void splitSprite() {
+        int width = m_sprite.getWidth(null);
+        int height = m_sprite.getHeight(null);
+        m_sprites = new BufferedImage[4 * 4];
+        m_w = width / 4;
+        m_h = height / 4;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int x = j * m_w;
+                int y = i * m_h;
 
-				m_sprites[(i * 4) + j] = m_sprite.getSubimage(x, y, m_w, m_h);
-			}
-		}
-	}
-
-	public void droite() {
-
-		Cellule cell = m_model.m_carte.cellules[m_y / 60][(m_x / 60) + 1];
+                m_sprites[(i * 4) + j] = m_sprite.getSubimage(x, y, m_w, m_h);
+            }
+        }
+    }
 
     public void droite() {
         if (m_x / Options.TAILLE_CELLULE != (Options.nb_px_x_max / Options.TAILLE_CELLULE - 1)) {
@@ -62,11 +59,10 @@ public class Personnage extends Entity {
     }
 
     public void haut() {
-        if (m_y/Options.TAILLE_CELLULE != 0) {
+        if (m_y / Options.TAILLE_CELLULE != 0) {
             Cellule cell = m_model.m_carte.cellules[(m_y / Options.TAILLE_CELLULE) - 1][m_x / Options.TAILLE_CELLULE];
             if (cell.libre) {
                 ramasser(cell);
-                i--;
                 m_y -= Options.TAILLE_CELLULE;
                 if (m_idx == 13)
                     m_idx = 14;
@@ -78,13 +74,12 @@ public class Personnage extends Entity {
     }
 
     public void bas() {
-        if (m_y/Options.TAILLE_CELLULE != ((Options.nb_px_y_max - Options.nb_px_y_min) / Options.TAILLE_CELLULE - 1)) {
+        if (m_y / Options.TAILLE_CELLULE != ((Options.nb_px_y_max - Options.nb_px_y_min) / Options.TAILLE_CELLULE - 1)) {
             Cellule cell = m_model.m_carte.cellules[(m_y / Options.TAILLE_CELLULE) + 1][m_x / Options.TAILLE_CELLULE];
             // int x = cell.x ;
             // int y = cell.y ;
             if (cell.libre) {
                 ramasser(cell);
-                i++;
                 m_y += Options.TAILLE_CELLULE;
                 if (m_idx == 2)
                     m_idx = 1;
@@ -96,12 +91,11 @@ public class Personnage extends Entity {
     }
 
     public void gauche() {
-        if (m_x / Options.TAILLE_CELLULE != 0){
+        if (m_x / Options.TAILLE_CELLULE != 0) {
             Cellule cell = m_model.m_carte.cellules[m_y / Options.TAILLE_CELLULE][(m_x / Options.TAILLE_CELLULE) - 1];
 
             if (cell.libre) {
                 ramasser(cell);
-                j--;
                 m_x -= Options.TAILLE_CELLULE;
                 if (m_idx == 6)
                     m_idx = 5;
@@ -112,83 +106,30 @@ public class Personnage extends Entity {
         }
     }
 
-		m_y-=60;
-
-		if(m_idx==13)
-			m_idx = 14;
-			else
-			m_idx=13;	
-		this.orientation=3;
-	}
-	}
-	public void bas() {
-	     
-		
-			Cellule cell = m_model.m_carte.cellules[(m_y / 60)+1][m_x / 60];
-           // int x = cell.x ;
-           // int y = cell.y ;
-	     	if(cell.libre) {	
-			
-			i++;
-		 	
-
-        }
-        */
-    public void paint(Graphics g) {
-
-        Image img = m_sprites[m_idx];
-        int w = (int) (m_scale * m_w);
-        int h = (int) (m_scale * m_h);
-        g.drawImage(img, m_x, m_y, w, h, null);
-
-    }
 
     public void ramasser(Cellule cell) {
-        Item[] item = m_model.m_item;
-        for (int i = 0; i < item.length; i++) {
-            if (item[i] != null) {
-                if (cell.x == item[i].w && cell.y == item[i].h && cell.entité!=null) {
-                    item[i].possession = 1;
-                    inventaire.enfiler(item[i]);
-                    cell.entité=null;
+        if(cell.entité!=null) {
+            Item[] item = m_model.m_item;
+            for (int i = 0; i < item.length; i++) {
+                if (item[i] != null) {
+                    if (cell.x == item[i].w && cell.y == item[i].h) {
+                        item[i].possession = 1;
+                        inventaire.enfiler(item[i]);
+                        cell.entité = null;
+                    }
                 }
             }
         }
 
     }
+    public void paint(Graphics g) {
 
-						if(m_idx==6)
-							m_idx = 5;
-						else
-							m_idx=6;	
-						this.orientation=2;
-		
-			}
-	}	
-/*	void change_orientation() {
-		
-		if(this.orientation == 0) {
-			m_idx=2;
-		}
-		if(this.orientation == 1) {
-			m_idx=10;
-		}
-		if(this.orientation == 2) {
-			m_idx=6;
-		}
-		if(this.orientation == 3) {
-			m_idx=13;
-		}
-			
-	}
-	*/
-	public void paint(Graphics g) {
-		
-			Image img = m_sprites[m_idx];
-			int w = (int) (m_scale * m_w);
-			int h = (int) (m_scale * m_h);
-			g.drawImage(img, m_x, m_y, w, h, null);
-		
-	}
+        Image img = m_sprites[m_idx];
+        int w = (int) (m_scale * m_w);
+        int h = (int) (m_scale * m_h);
+        g.drawImage(img, m_x+10, m_y, w, h, null);
+
+    }
+
 
 }
